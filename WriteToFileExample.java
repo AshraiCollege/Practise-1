87aecb5e777e798da6d575b85e1e81916c28c3ab
Ashrai <ashraicselab@gmail.com>
2025-07-02 10:30:05 +0600

    version1 on branch2

--- WriteToFileExample.java
+++ WriteToFileExample.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 public class WriteToFileExample {
 
     public static void main(String[] args) {
-        String fileName = "output.txt";
-        String contentToWrite = "This is a line of text that will be written to the file.\nAnother line follows.";
+        String fileName = "output2.txt";
+        String contentToWrite = "12213343324 2344234234 12334123.\nAnother line follows.";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             writer.write(contentToWrite);
-            writer.write("\nI am from Ashrai Engineering College\n");
+            writer.write("\nCSE\nEEE\n");
               writer.write("\nHi, I am from Ashrai Engineering College\n");
             System.out.println("Successfully wrote to the file: " + fileName);
         } catch (IOException e) {
